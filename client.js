8c5bc3a26719ec9fe826100e2f4418c0fd825f56
Daniel Ridley <ridleydan31@gmail.com>
2018-08-31 17:00:03 -0500

    this will loop through salary array

--- client.js
+++ client.js
@@ -1,26 +1,45 @@
 console.log('js');
 
 $(document).ready(readyNow)
+let salary =[];
 
-function readyNow(){
-    $('#submitButton').on('click', submitButton)    
+function readyNow() {
+    $('#submitButton').on('click', addEmployee);
+    $('#submitButton').on('click', handleClick);
 }//end readys
 
-function handleClick (){
-    let newFirstName =$('#firstNIn').val()
+function handleClick() {
+    let newFirstName = $('#firstNIn').val()
+    let newLastName = $('#lastNIn').val()
+    let newId = $('#iDIn').val()
+    let newTitle = $('#titleIn').val()
+    let newAnnSal = $('#annSalIn').val()
 
     $('#empTableBody').append(
     `<tr>
-        <td> `+ newFirstName +`</td>
-        <td>Last Name</td>
-        <td>ID</td>
-        <td>Title</td>
-        <td>Annual Salary</td>
+        <td> `+ newFirstName + ` </td>
+        <td>` + newLastName + ` </td>
+        <td>`+ newId + ` </td>
+        <td>`+ newTitle + ` </td>
+        <td>`+ newAnnSal + ` </td>
+        <td><button id="removeEmp">Delete</button></td>
     </tr>`
-)
+    )
+    salary.push(newAnnSal);
+    let totalSalary = 0
+    for (let i = 0; i < salary.length; i++) {      
+    }
+    $('#monthlyTotal').;
+
+    $('#firstNIn').val('')
+    $('#lastNIn').val('')
+    $('#iDIn').val('')
+    $('#titleIn').val('')
+    $('#annSalIn').val('')
+
 }
 
-function submitButton() {
-    console.log('working');    
+function addEmployee() {
+    console.log('working');
 }//end submitButton
 
