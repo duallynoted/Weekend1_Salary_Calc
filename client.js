5946f6c9b15116964bc958a418337d0f4bbc792d
Daniel Ridley <ridleydan31@gmail.com>
2018-08-31 16:01:34 -0500

    this will append name to a table

--- client.js
+++ client.js
@@ -3,5 +3,24 @@ console.log('js');
 $(document).ready(readyNow)
 
 function readyNow(){
-    console.log('JQ');    
+    $('#submitButton').on('click', submitButton)    
+}//end readys
+
+function handleClick (){
+    let newFirstName =$('#firstNIn').val()
+
+    $('#empTableBody').append(
+    `<tr>
+        <td> `+ newFirstName +`</td>
+        <td>Last Name</td>
+        <td>ID</td>
+        <td>Title</td>
+        <td>Annual Salary</td>
+    </tr>`
+)
 }
+
+function submitButton() {
+    console.log('working');    
+}//end submitButton
+
