0eab7fd9e732d0f67bf089b1dc4b9a2eb3d32226
Daniel Ridley <ridleydan31@gmail.com>
2018-09-01 16:46:05 -0500

    this will style the table

--- client.js
+++ client.js
@@ -1,11 +1,14 @@
 console.log('js');
 
 $(document).ready(readyNow)
-let salary =[];
+let salary = [];
+let totalSalary = 1
 
 function readyNow() {
     $('#submitButton').on('click', addEmployee);
     $('#submitButton').on('click', handleClick);
+    $('#empTableBody').on('click','.removeEmp', handleDelete);
+    addSalaries();
 }//end readys
 
 function handleClick() {
@@ -16,20 +19,18 @@ function handleClick() {
     let newAnnSal = $('#annSalIn').val()
 
     $('#empTableBody').append(
-    `<tr>
+        `<tr>
         <td> `+ newFirstName + ` </td>
         <td>` + newLastName + ` </td>
         <td>`+ newId + ` </td>
         <td>`+ newTitle + ` </td>
         <td>`+ newAnnSal + ` </td>
-        <td><button id="removeEmp">Delete</button></td>
+        <td><button class="removeEmp">Delete</button></td>
     </tr>`
     )
     salary.push(newAnnSal);
-    let totalSalary = 0
-    for (let i = 0; i < salary.length; i++) {      
-    }
-    $('#monthlyTotal').;
+    totalSalary = addSalaries();
+    $('#totalSalary').text('Total Monthly:' + '$' + totalSalary);
 
     $('#firstNIn').val('')
     $('#lastNIn').val('')
@@ -40,6 +41,23 @@ function handleClick() {
 }
 
 function addEmployee() {
-    console.log('working');
+    console.log('Adds Employee');
 }//end submitButton
+function addSalaries() {
+    let subTotal = 0
+    for (let i = 0; i < salary.length; i++) {
+        subTotal = subTotal + parseInt(salary[i]) / 12;
+    }//end for loop 
+    if (subTotal > 20000) {
+        console.log('>$20,000');
+        $('#totalSalary').css('background-color', 'red')
+
+    }//end conditional 
+    return subTotal;
+
+}//end addSalaries
+function handleDelete(){
+    console.log('delete was clicked');
+    $(this).parent().parent().remove();
+}
 
